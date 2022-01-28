dbfe15983404d50a77cd2c337270680eb8b90776
Sphinx <quyuan2008@163.com>
2022-01-28 13:33:19 -0700

    randomize hat tint

--- Assets/Scripts/HatSpawning.cs
+++ Assets/Scripts/HatSpawning.cs
@@ -12,10 +12,17 @@ public class HatSpawning : MonoBehaviour
     float timeInterval = 0f;
 
     float timeGap;
+    Color[] hatColors = new Color[4];
+
     // Start is called before the first frame update
     void Start()
     {
         timeGap = Random.Range( 0.6f, 3.5f);
+
+        ColorUtility.TryParseHtmlString("#3768A7",out hatColors[0]);
+        ColorUtility.TryParseHtmlString("#7637A7", out hatColors[1]);
+        ColorUtility.TryParseHtmlString("#A7A037", out hatColors[2]);
+        ColorUtility.TryParseHtmlString("#FFFFFF", out hatColors[3]); //original
     }
 
     // Update is called once per frame
@@ -35,7 +42,8 @@ public class HatSpawning : MonoBehaviour
     public void SpawnHat()
     {
         Vector3 pos = transform.localPosition + center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-        Instantiate(hatPrefab, pos, Quaternion.identity);
+        GameObject hat = Instantiate(hatPrefab, pos, Quaternion.identity);
+        hat.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = hatColors[Random.Range(0, hatColors.Length - 1)];
     }
 
     void OnDrawGizmosSelected()

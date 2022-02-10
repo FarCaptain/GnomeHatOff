0d402edc0a0b22d859766671647a158c63f40606
RuijiaHuang <965990240@qq.com>
2022-02-10 13:26:35 -0700

    Ruijia: Integrate two Arduinoreceiver scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-#define KEYBOARD
+//#define KEYBOARD
 //else use alt controller
 
 using System.Collections;
@@ -21,53 +21,39 @@ public class PlayerMovement : MonoBehaviour
     // to filter the value we get from the gyroscope
     public int delayedFrames;
     private int remainingFrames;
-    private float prevLRSign;
+  
     private Rigidbody rigidBody;
 
     public ParticleSystem runDust;
 
     public Vector3 shownSpeed;
 
-#if KEYBOARD
-    public float keyboardSpeed;
-#else
-    public float xval = ArduinoReceiver.xaxis;
-    public float zval = ArduinoReceiver.zaxis;
-#endif
 
-    Vector2 initPos;        // new default position for controller when calibrated
-    bool ifInit;
+    public float keyboardSpeed;
+
+    public float xval;
+
+
+    Vector3 initPos = new Vector3(0,0,0);        // new default position for controller when calibrated
+ 
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
 
-#if KEYBOARD
-        ifInit = true;
-#else
-        //speed = 5f;
-        ifInit = false;
 
-        remainingFrames = delayedFrames;
-#endif
+
+
     }
 
     void FixedUpdate()
     {
-#if KEYBOARD
-#else
-        // used for calibration
-        if (Input.GetKey(KeyCode.Q))
-        {
-            initPos = new Vector2(ArduinoReceiver.xaxis, ArduinoReceiver.zaxis);
-            ifInit = true;
-            prevLRSign = 0;
-        }
-#endif
 
-        if (ifInit)
-        {
-#if KEYBOARD
+        // used for calibration
+
+
+
+
             Vector3 move = Vector3.zero;
             if(playerIndex == 0)
             {
@@ -91,43 +77,15 @@ public class PlayerMovement : MonoBehaviour
            
 
             Vector3 speed = move * keyboardSpeed;
-#else
-
-            xval = ArduinoReceiver.xaxis - initPos.x;
-            zval = ArduinoReceiver.zaxis - initPos.y;
-
-            Vector3 move = new Vector3(xval, 0f, zval);
-
-            // adjusts speed according to our thresholds for an acceleration effect
-            float speed_x, speed_z;
-            if (Mathf.Abs(move.x) <= MinthresholdLR)
-                speed_x = 0f;
-            else if (Mathf.Abs(move.x) >= MaxthresholdLR)
-                speed_x = maxSpeed;
-            else 
-                speed_x = map(Mathf.Abs(move.x), MinthresholdLR, MaxthresholdLR, minSpeed, maxSpeed);
-
-            if (Mathf.Abs(move.z) <= MinthresholdFB)
-                speed_z = 0f;
-            else if (Mathf.Abs(move.z) >= MaxthresholdFB)
-                speed_z = maxSpeed;
-            else
-                speed_z = map(Mathf.Abs(move.z), MinthresholdFB, MaxthresholdFB, minSpeed, maxSpeed);
-
-            // assigns direction
-            speed_x *= Mathf.Sign(move.x);
-            speed_z *= Mathf.Sign(move.z);
-            Vector3 speed = new Vector3(speed_x, 0f, speed_z);
-#endif
-
             if (speed != Vector3.zero)
             {
                 gameObject.transform.forward = speed;
-                drawRunDust( );
+                Move(speed * Time.deltaTime);
+            drawRunDust( );
             }
 
-            Move(speed * Time.deltaTime);
-        }
+           
+
         // keeps object from flying off (might be removable)
         Vector3 pos = gameObject.transform.position;
         gameObject.transform.position = new Vector3(pos.x, 0.1f, pos.z);
@@ -136,8 +94,50 @@ public class PlayerMovement : MonoBehaviour
     private void Move(Vector3 motion)
     {
         rigidBody.velocity = motion;
+        Debug.Log(motion);
     }
-    
+
+    public void Move(float xaxis, float yaxis,float zaxis)
+    {
+        float xval = xaxis - initPos.x;
+        float yval = yaxis - initPos.y;
+        float zval = zaxis - initPos.y;
+
+        Vector3 move = new Vector3(xval, 0f, zval);
+
+        // adjusts speed according to our thresholds for an acceleration effect
+        float speed_x, speed_z;
+        if (Mathf.Abs(move.x) <= MinthresholdLR)
+            speed_x = 0f;
+        else if (Mathf.Abs(move.x) >= MaxthresholdLR)
+            speed_x = maxSpeed;
+        else
+            speed_x = map(Mathf.Abs(move.x), MinthresholdLR, MaxthresholdLR, minSpeed, maxSpeed);
+
+        if (Mathf.Abs(move.z) <= MinthresholdFB)
+            speed_z = 0f;
+        else if (Mathf.Abs(move.z) >= MaxthresholdFB)
+            speed_z = maxSpeed;
+        else
+            speed_z = map(Mathf.Abs(move.z), MinthresholdFB, MaxthresholdFB, minSpeed, maxSpeed);
+
+        // assigns direction
+        speed_x *= Mathf.Sign(move.x);
+        speed_z *= Mathf.Sign(move.z);
+        Vector3 speed = new Vector3(speed_x, 0f, speed_z);
+        //Debug.Log(speed);
+
+        if (speed != Vector3.zero)
+        {
+            gameObject.transform.forward = speed;
+            drawRunDust();
+        }
+        Move(speed * Time.deltaTime);
+        Vector3 pos = gameObject.transform.position;
+        Debug.Log(pos);
+        gameObject.transform.position = new Vector3(pos.x, 0.1f, pos.z);
+    }
+
     private void drawRunDust()
     {
         if (!runDust.isPlaying)
@@ -150,5 +150,10 @@ public class PlayerMovement : MonoBehaviour
     private float map(float s, float a1, float a2, float b1, float b2)
     {
         return (s - a1) * (b2 - b1) / (a2 - a1) + b1;
+    }
+
+    public void SetOffset(Vector3 i_offset)
+    {
+        initPos = i_offset;
     }
 }

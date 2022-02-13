261cae0da90ad9bc95bf370a0be6b48ae9a58bc1
Hisham Jaffar <hishamjaffar98@gmail.com>
2022-02-13 01:43:44 -0700

    Added a Dev Mode Debug Script to allow for easier Testing

---
+++ Assets/Scripts/DevModeDebug.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevModeDebug : MonoBehaviour
+{
+    HatCollecter[] hatCollecters;
+    void Start()
+    {
+        hatCollecters = FindObjectsOfType<HatCollecter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Debug.isDebugBuild)
+		{
+			UseMKeyForDrop();
+		}
+	}
+
+	private void UseMKeyForDrop()
+	{
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			for (int i = 0; i < hatCollecters.Length; i++)
+			{
+				hatCollecters[i].hatdrop = true;
+			}
+		}
+	}
+}

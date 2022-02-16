cc55864c3862520d6bcc6eb569fb1a34a795e70d
Hisham Jaffar <40393489+HishamJaffar98@users.noreply.github.com>
2022-02-15 19:23:27 -0700

    Implemented Log Roll Mechanic

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    [Header("Mobility Variables")]
+    [SerializeField] float movementSpeed = 0.5f;
+    [SerializeField] float rotationSpeed = 100f;
+
+    [Header("Direction Variables")]
+    [SerializeField] bool xAxis;
+    [SerializeField] bool zAxis;
+    [SerializeField] int reverseX = 1;
+    [SerializeField] int reverseZ = 1;
+
+    private GameObject parentGameObject;
+
+    void Start()
+    {
+        parentGameObject = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0));
+        if (xAxis)
+		{
+            transform.position = new Vector3(transform.position.x + (movementSpeed * Time.deltaTime * reverseX), transform.position.y, transform.position.z);
+        }
+        if(zAxis)
+		{
+            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + (movementSpeed * Time.deltaTime * reverseZ));
+        }
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "LogDestroyer")
+		{
+            parentGameObject.GetComponent<LogSpawner>().logSpawnTimer.TimerStart = true;
+            parentGameObject.GetComponent<LogSpawner>().logAlive = false;
+            Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LogSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogSpawner : MonoBehaviour
+{
+    [SerializeField] float minTimeBetweenSpawns;
+    [SerializeField] float maxTimeBetweenSpawns;
+    [HideInInspector]
+    public bool logAlive = false;
+
+    [SerializeField] GameObject logToSpawn;
+    [HideInInspector]
+    public NewTimer logSpawnTimer;
+
+    void Start()
+	{
+        InitializeLogSpawnTimer();
+        SpawnLog();
+    }
+
+	private void InitializeLogSpawnTimer()
+	{
+		logSpawnTimer = gameObject.AddComponent<NewTimer>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (logSpawnTimer.TimerStart == false && logAlive == false && logSpawnTimer!=null)
+        {
+            SpawnLog();
+        }
+    }
+
+    public void SpawnLog()
+	{
+        logAlive = true;
+        logSpawnTimer.MaxTime = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+        GameObject logSpawned = Instantiate(logToSpawn, transform.position, logToSpawn.transform.rotation);
+        logSpawned.transform.SetParent(transform);
+	}
+}

6136b314bf8e94e21a2a3d68f2b3d9302e977001
Ivan Lee <ivan.trinh.lee@gmail.com>
2022-02-28 02:27:41 -0700

    UI stuff
    Pls don't pull from this branch lmao

---
+++ Assets/UIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit)");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Map1Level Layout");
+
+    }
+}

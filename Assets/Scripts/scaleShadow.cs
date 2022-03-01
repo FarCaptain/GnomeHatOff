c6e09b18067e0797d694e7f10e635e7e90185905
Pranav Bhatt <bhattpranav19@gmail.com>
2022-02-28 21:40:02 -0700

    fixed attraction logic

--- Assets/Scripts/scaleShadow.cs
+++ Assets/Scripts/scaleShadow.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class scaleShadow : MonoBehaviour
 {
     public float scaleSpeed = 0.2f;
     bool attraction;
     GameObject player;
-    float MagnetismStrength = 3f;
+    float MagnetismStrength = 4f;
     float MagnetismReductionOverTime = 0.1f;
     float MinimumSnapValue = 0.08f;
     float Timer = 1;
@@ -15,7 +16,8 @@ public class scaleShadow : MonoBehaviour
     void Start()
     {
         attraction = false;
-        transform.localScale = new Vector3(0, 0, 0); 
+        transform.localScale = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
@@ -27,18 +29,22 @@ public class scaleShadow : MonoBehaviour
         }
         if(player)
         {
-            if (attraction == true)
+            if (attraction == true )
             {
                 Vector3 direction = (this.transform.position - player.transform.position).normalized;
-                   // Debug.Log(direction);
-                player.GetComponent<Rigidbody>().AddForce(direction * MagnetismStrength, ForceMode.Force);
+                // Debug.Log(direction);
+                Vector3 Finaldir = new Vector3(Mathf.Abs(direction.x), Mathf.Abs(direction.y), Mathf.Abs(direction.z));
+
+                player.GetComponent<Rigidbody>().AddForce((Finaldir) * MagnetismStrength, ForceMode.Force) ;
                 if(MagnetismStrength > 0)
                 MagnetismStrength -= MagnetismReductionOverTime;
-/*                if ((direction.x >= -MinimumSnapValue && direction.x <= MinimumSnapValue) || (direction.z >= -MinimumSnapValue && direction.z <= MinimumSnapValue))
-                {
-                    player.transform.position = this.transform.position;
-                    attraction = false;
-                }*/
+                attraction = false;
+                /*                if ((direction.x >= -MinimumSnapValue && direction.x <= MinimumSnapValue) || (direction.z >= -MinimumSnapValue && direction.z <= MinimumSnapValue))
+                                {
+                                    player.transform.position = this.transform.position;
+                                    attraction = false;
+                                }*/
+
             }
         }
     }

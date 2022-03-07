bc74c60392c1365c95525bcc7cb5632051733d93
Hisham Jaffar <hishamjaffar98@gmail.com>
2022-03-07 11:13:50 -0700

    Added start menu

---
+++ Assets/Hisham_UI_Stuff/Scripts/StartMenuButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StartMenuButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] float buttonInitialXPos;
+    [SerializeField] float buttonFinalXPos;
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(buttonFinalXPos, gameObject.GetComponent<RectTransform>().anchoredPosition.y);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(buttonInitialXPos, gameObject.GetComponent<RectTransform>().anchoredPosition.y);
+    }
+}
--- Assets/Hisham_UI_Stuff/Scripts/StartMenuUIManager.cs
+++ Assets/Hisham_UI_Stuff/Scripts/StartMenuUIManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class StartMenuUIManager : MonoBehaviour
 {
+    [SerializeField] float buttonInitialXPos;
+    [SerializeField] float buttonFinalXPos;
     [SerializeField] GameObject fader;
+    [SerializeField] Animator responsiveModelAnimator;
     void Start()
     {
         fader.GetComponent<Animator>().SetBool("isFadingIn", true);
@@ -15,4 +20,39 @@ public class StartMenuUIManager : MonoBehaviour
     {
         
     }
+
+    public void ActivateOnHoverAnimation()
+	{
+        responsiveModelAnimator.SetBool("hoverOverStart", true);
+    }
+
+    public void ActivateOffHoverAnimation()
+    {
+        responsiveModelAnimator.SetBool("hoverOverStart", false);
+    }
+
+    public void ActivateOnClickAnimation()
+	{
+        responsiveModelAnimator.SetTrigger("startButtonClicked");
+	}
+
+    public void ButtonHoverTransform()
+	{
+        GameObject buttonHoveredOver = EventSystem.current.currentSelectedGameObject;
+		if (buttonHoveredOver.GetComponent<Button>())
+		{
+            buttonHoveredOver.GetComponent<RectTransform>().localPosition = new Vector2(buttonFinalXPos, buttonHoveredOver.GetComponent<RectTransform>().localPosition.y);
+		}
+	}
+
+    public void ButtonIdleTransform()
+	{
+        GameObject buttonHoveredOver = EventSystem.current.currentSelectedGameObject;
+        if (buttonHoveredOver.GetComponent<Button>())
+        {
+            buttonHoveredOver.GetComponent<RectTransform>().localPosition = new Vector2(buttonInitialXPos, buttonHoveredOver.GetComponent<RectTransform>().localPosition.y);
+        }
+    }
+
+
 }

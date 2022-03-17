b43179d757155d29c493b88d996159621815fe5c
Ruijia Huang <rjhuang1999@gamil.com>
2022-03-17 17:46:35 -0600

    Ruijia: Add Plane

---
+++ Assets/IcePlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcePlane : MonoBehaviour
+{
+    Vector3 scale;
+    public float time;
+    public float speed;
+    float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = GetComponent<Transform>().localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        GetComponent<Transform>().localScale -= GetComponent<Transform>().localScale*(speed*Time.deltaTime*0.01f);
+        if (t > time)
+        {
+            t = 0;
+         //   Shrink();
+        }
+    }
+
+    void Shrink()
+    {
+        
+        scale = GetComponent<Transform>().localScale;
+    }
+}

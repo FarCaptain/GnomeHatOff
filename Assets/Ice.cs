e08a3a22fc62bb5f2b49a434445a212a205b3241
Ruijia Huang <rjhuang1999@gamil.com>
2022-03-18 00:19:05 -0600

    Ruijia: Add melting and shrinking

---
+++ Assets/Ice.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice : MonoBehaviour
+{
+    Animation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Melt()
+    {
+        float waitTime = Random.Range(0, 1.5f);
+        Invoke("PlayMeltAnimation",waitTime);
+        
+    }
+
+    void PlayMeltAnimation()
+    {
+        
+        animation.Play("IceMelting");
+        Invoke("DestroyGameObject", 2);
+        
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+}

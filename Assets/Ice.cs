a741eeb5b8c987d5b1a201fb71e9cf1d31b4d1ef
RuijiaHuang <965990240@qq.com>
2022-03-31 12:59:01 -0600

    Add player standing

--- Assets/Ice.cs
+++ Assets/Ice.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Ice : MonoBehaviour
 {
-    private int health;
+    private float health;
 
     [SerializeField]
     private Material[] meltMaterials;
     int stageNum;
 
+    public float meltTime;
+    float playerOnMeltTime;
+    bool hasPlayerOn;
     bool isSink;
+    bool isHit;
     Animation animation;
     GameObject go_Ice;
     bool isInitialized = false;
@@ -22,20 +27,49 @@ public class Ice : MonoBehaviour
         stageNum = meltMaterials.Length;
         health = 0;
         isSink = false;
-        go_IceMat = transform.Find("Ice/pasted__group7/pasted__pasted__pCylinder2/polySurface11").gameObject; 
-       
-        animation = GetComponent<Animation>();
+        hasPlayerOn = false;
+        go_IceMat = transform.Find("Ice/pasted__group7/pasted__pasted__pCylinder2/polySurface11").gameObject;
+        playerOnMeltTime = 0;
+         animation = GetComponent<Animation>();
         
     }
 
+    private void FixedUpdate()
+    {
+        if (isHit&&hasPlayerOn)
+        {
+            health -= 2*Time.fixedDeltaTime;
+        }else if (hasPlayerOn && !isHit)
+        {
+            health -= Time.fixedDeltaTime;
+        }
+        else if (!hasPlayerOn && isHit)
+        {
+            health -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            health += Time.fixedDeltaTime;
+        }
+
+        //int stage = (int)Math.Floor(health);
+        //if (health == stageNum)
+        //{
+        //    DestroyGameObject();
+        //    return;
+        //}
+
+
+        //go_IceMat.GetComponent<MeshRenderer>().material = meltMaterials[stage];
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("Knockback"))
         {
-            Debug.Log("HIT");
+            
             if (!collision.gameObject.GetComponent<Snowball>().Hit)
             {
-                Crack();
+                //Crack();
                 collision.gameObject.GetComponent<Snowball>().Hit = true;
             }
         }
@@ -47,9 +81,25 @@ public class Ice : MonoBehaviour
         {
             if (go_Ice != null)
             {
+                hasPlayerOn = true;
                // Down();
             }
           
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag.Equals("Player"))
+        {
+            if (go_Ice != null)
+            {
+                hasPlayerOn = true;
+                playerOnMeltTime += 0.01f;
+                // Down();
+            }
+
         }
     }
     private void OnTriggerExit(Collider other)
@@ -59,7 +109,9 @@ public class Ice : MonoBehaviour
         {
             if (go_Ice != null)
             {
-              //  Up();
+                hasPlayerOn = false;
+                playerOnMeltTime = 0;
+                //  Up();
             }
 
         }

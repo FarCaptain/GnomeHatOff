ccc50d88667d3871c3868edd4ccd47424d5aa276
leeiv <44705038+leeiv@users.noreply.github.com>
2022-04-03 21:46:43 -0600

    colorblind mode implemented

---
+++ Assets/TextMesh Pro/Colorblind/ThreeDColorBlind.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ThreeDColorBlind : MonoBehaviour
+{
+    public Renderer render;
+    public Material colorblindMaterial;
+    public Material colorblindMaterialHat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("ToggleBool2") == 1)
+        {
+            render.material = colorblindMaterial;
+            var materials = render.materials;
+            materials[4] = colorblindMaterialHat;
+            render.materials = materials;
+        }
+        
+    }
+}

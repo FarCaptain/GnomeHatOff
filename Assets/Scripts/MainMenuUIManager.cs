f72fe52b6716ccd39c9663bd257c6ddacc755736
Hisham Jaffar <hishamjaffar98@gmail.com>
2022-04-14 12:31:58 -0600

    Main Menu Update

---
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement ;
+using TMPro;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [SerializeField] LevelManager levelManager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuickStartButtonClicked()
+	{
+        levelManager.LoadScene(2);
+
+    }
+    IEnumerator WaitAndLoad(int sceneIndex)
+	{
+        //confirmationText.text = "LEVEL SELECT LOADING";
+        yield return new WaitForSecondsRealtime(1f);
+        SceneManager.LoadScene(2);
+	}
+}

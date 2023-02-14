80cb366e995b8d456a1f3e9f337aa80954b9f944
pranav-bt <89481021+pranav-bt@users.noreply.github.com>
2023-02-13 22:09:36 -0700

    Added simple UI and two players only

---
+++ Assets/SimpleUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleUI : MonoBehaviour
+{
+    public void LoadForest1()
+    {
+        SceneManager.LoadScene("Map1Level Layout");
+    }
+
+    public void LoadForest2()
+    {
+        SceneManager.LoadScene("Map1Level2 Layout");
+    }
+
+    public void LoadIce1()
+    {
+        SceneManager.LoadScene("IceMapLayout");
+    }
+
+    public void LoadIce2()
+    {
+        SceneManager.LoadScene("IceMap2Layout");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("Normal Level UI");
+    }
+}
